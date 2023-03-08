f013db2d6613a962270ba4b0ae6382c01e46d125
BAO-1987 <andriy.bezkorovajnyj@gmail.com>
2023-03-08 16:19:23 +0200

    ffff

--- src/js/components/_count.js
+++ src/js/components/_count.js
@@ -1,58 +1,5 @@
-// /***** CALCULATE THE TIME REMAINING *****/
-// function getTimeRemaining(endtime) {
-//   let t = Date.parse(endtime) - Date.parse(new Date());
 
-//   /***** CONVERT THE TIME TO A USEABLE FORMAT *****/
-//   let seconds = Math.floor((t / 1000) % 60);
-//   let minutes = Math.floor((t / 1000 / 60) % 60);
-//   let hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-//   let days = Math.floor(t / (1000 * 60 * 60 * 24));
-
-//   /***** OUTPUT THE CLOCK DATA AS A REUSABLE OBJECT *****/
-//   return {
-//     'total': t,
-//     'days': days,
-//     'hours': hours,
-//     'minutes': minutes,
-//     'seconds': seconds
-//   };
-// }
-
-// /***** DISPLAY THE CLOCK AND STOP IT WHEN IT REACHES ZERO *****/
-// function initializeClock(id, endtime) {
-//   let clock = document.getElementById(id);
-//   let daysSpan = clock.querySelector('.days');
-//   let hoursSpan = clock.querySelector('.hours');
-//   let minutesSpan = clock.querySelector('.minutes');
-//   let secondsSpan = clock.querySelector('.seconds');
-
-//   function updateClock() {
-//     const t = getTimeRemaining(endtime);
-
-//     daysSpan.innerHTML = '0' + t.days;
-//     hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-//     minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-//     secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-
-//     if (t.total <= 0) {
-//       clearInterval(timeinterval);
-//       document.getElementById("clockdiv").style.display = "none";
-//     }
-//   }
-
-//   updateClock(); // run function once at first to avoid delay
-//   let timeinterval = setInterval(updateClock, 1000);
-// }
-
-// /***** SET A VALID END DATE *****/
-// const deadline = new Date(Date.parse(new Date())  + 1 * 1 * 1 * 10 * 1000);
-// initializeClock('clockdiv', deadline);
-
-
-
-
-// my next birthday
-const newDate = new Date('mar 07 23 20:29:25').getTime()
+const newDate = new Date('mar 11 23 20:29:25').getTime()
 const countdown = setInterval(() => {
 
   const date = new Date().getTime()
