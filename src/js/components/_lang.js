44f800ac32e5900381f9d43bb9f3154b1bcca759
BAO-1987 <andriy.bezkorovajnyj@gmail.com>
2023-03-27 21:26:50 +0300

    ddddd

---
+++ src/js/components/_lang.js
@@ -0,0 +1,9 @@
+document.addEventListener("click", (e) => {
+  if (!e.target.matches(".languages__span")) {
+    document
+      .querySelector(".languages__list")
+      .classList.remove("open");
+  } else {
+    e.target.nextElementSibling.classList.add("open");
+  }
+});
