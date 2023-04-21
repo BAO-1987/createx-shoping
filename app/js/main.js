87ee022a5682718b34d87e431f87b59a37cc6ce3
BAO-1987 <andriy.bezkorovajnyj@gmail.com>
2023-04-21 23:00:17 +0300

    breadcrunb

--- app/js/main.js
+++ app/js/main.js
@@ -300,15 +300,17 @@ showPasswordCheckbox.addEventListener('change', function () {
   \*****************************************/
 /***/ (() => {
 
-const modal = document.querySelector("#size");
-const openModal = document.querySelector(".btn__size");
-const closeModal = document.querySelector(".size__close");
-openModal.addEventListener("click", () => {
-  modal.showModal();
-});
-closeModal.addEventListener("click", () => {
-  modal.close();
-});
+// const modal = document.querySelector("#size");
+// const openModal = document.querySelector(".btn__size");
+// const closeModal = document.querySelector(".size__close");
+
+// openModal.addEventListener("click", () => {
+//   modal.showModal();
+// });
+
+// closeModal.addEventListener("click", () => {
+//   modal.close();
+// });
 
 /***/ }),
 
