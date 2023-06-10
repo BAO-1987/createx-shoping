0b4695f9794083e1ad8b4eb71eb98c19dc40ca92
BAO-1987 <andriy.bezkorovajnyj@gmail.com>
2023-06-10 18:27:09 +0300

    modal

--- src/js/components/_modal.js
+++ src/js/components/_modal.js
@@ -1,10 +1,15 @@
-const modal = document.querySelector("#modal");
-const openModal = document.querySelector(".login__btn");
-const closeModal = document.querySelector(".modal__close");
+const modal = document.querySelector("[data-modal]");
+const openModal = document.querySelector("[data-modal-open]");
+const openModalSing = document.querySelector("[data-modal-open-sing]");
+const closeModal = document.querySelector("[data-modal-close]");
 
 openModal.addEventListener("click", () => {
   modal.showModal();
 });
+
+// openModalSing.addEventListener("click", () => {
+//   modal.showModal();
+// });
 
 closeModal.addEventListener("click", () => {
   modal.close();
--- src/js/components/_sizeModal.js
+++ src/js/components/_sizeModal.js
@@ -1,11 +1,11 @@
-const modal = document.querySelector("#size-modal");
-const openModal = document.querySelector(".btn__size");
-const closeModal = document.querySelector(".size__close");
+// const modal = document.querySelector("#size-modal");
+// const openModal = document.querySelector(".btn__size");
+// const closeModal = document.querySelector(".size__close");
 
-openModal.addEventListener("click", () => {
-  modal.showModal();
-});
+// openModal.addEventListener("click", () => {
+//   modal.showModal();
+// });
 
-closeModal.addEventListener("click", () => {
-  modal.close();
-});
+// closeModal.addEventListener("click", () => {
+//   modal.close();
+// });
