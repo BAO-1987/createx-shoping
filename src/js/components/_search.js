7af6656bb21770385c18f583d7291b5e91900239
BAO-1987 <andriy.bezkorovajnyj@gmail.com>
2023-06-16 22:39:08 +0300

    ffs

---
+++ src/js/components/_search.js
@@ -0,0 +1,7 @@
+function focusInputField() {
+  var inputField = document.getElementById("search");
+  inputField.focus();
+
+  var myDiv = document.getElementById("headerAction");
+  myDiv.style.display = "none";
+}
