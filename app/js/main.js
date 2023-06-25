b858bf3a1cfcb84db2967be5c80707d9e52da27c
BAO-1987 <andriy.bezkorovajnyj@gmail.com>
2023-06-25 18:48:36 +0300

    ddsss

--- app/js/main.js
+++ app/js/main.js
@@ -546,11 +546,32 @@ showPasswordCheckboxes.forEach(function (checkbox) {
   \**************************************/
 /***/ (() => {
 
+// // (function () {
+// //   const logo = document.querySelector('[data-logo]');
+// //   const headerAction = document.querySelector('[data-hide]');
+// //   const formSearch = document.querySelector('[data-search]');
+// //   const formClose = document.querySelector('[data-close]');
+
+// //   formSearch?.addEventListener('click', function (e) {
+// //     formSearch.classList.toggle('open');
+// //     headerAction.classList.toggle('hidden');
+// //     logo.classList.toggle('hidden');
+// //   });
+
+// //   formClose?.forEach(el => {
+// //     el.addEventListener('click', () => {
+// //       logo.classList.remove('hidden');
+// //       headerAction.classList.remove('hidden');
+// //       formSearch.classList.remove('open');
+// //     });
+// //   });
+// // })();
+
 // (function () {
 //   const logo = document.querySelector('[data-logo]');
 //   const headerAction = document.querySelector('[data-hide]');
 //   const formSearch = document.querySelector('[data-search]');
-//   const formClose = document.querySelector('[data-close]');
+//   const formClose = document.querySelector('form-search__close');
 
 //   formSearch?.addEventListener('click', function (e) {
 //     formSearch.classList.toggle('open');
@@ -558,13 +579,13 @@ showPasswordCheckboxes.forEach(function (checkbox) {
 //     logo.classList.toggle('hidden');
 //   });
 
-//   formClose?.forEach(el => {
-//     el.addEventListener('click', () => {
+//   if (formClose) {
+//     formClose.addEventListener('click', function () {
 //       logo.classList.remove('hidden');
 //       headerAction.classList.remove('hidden');
 //       formSearch.classList.remove('open');
 //     });
-//   });
+//   }
 // })();
 
 (function () {
@@ -577,13 +598,11 @@ showPasswordCheckboxes.forEach(function (checkbox) {
     headerAction.classList.toggle('hidden');
     logo.classList.toggle('hidden');
   });
-  if (formClose) {
-    formClose.addEventListener('click', function () {
-      logo.classList.remove('hidden');
-      headerAction.classList.remove('hidden');
-      formSearch.classList.remove('open');
-    });
-  }
+  formClose?.addEventListener('click', function (e) {
+    logo.classList.remove('hidden');
+    headerAction.classList.remove('hidden');
+    formSearch.classList.remove('open');
+  });
 })();
 
 /***/ }),
--- src/js/components/_search.js
+++ src/js/components/_search.js
@@ -1,8 +1,29 @@
+// // (function () {
+// //   const logo = document.querySelector('[data-logo]');
+// //   const headerAction = document.querySelector('[data-hide]');
+// //   const formSearch = document.querySelector('[data-search]');
+// //   const formClose = document.querySelector('[data-close]');
+
+// //   formSearch?.addEventListener('click', function (e) {
+// //     formSearch.classList.toggle('open');
+// //     headerAction.classList.toggle('hidden');
+// //     logo.classList.toggle('hidden');
+// //   });
+
+// //   formClose?.forEach(el => {
+// //     el.addEventListener('click', () => {
+// //       logo.classList.remove('hidden');
+// //       headerAction.classList.remove('hidden');
+// //       formSearch.classList.remove('open');
+// //     });
+// //   });
+// // })();
+
 // (function () {
 //   const logo = document.querySelector('[data-logo]');
 //   const headerAction = document.querySelector('[data-hide]');
 //   const formSearch = document.querySelector('[data-search]');
-//   const formClose = document.querySelector('[data-close]');
+//   const formClose = document.querySelector('form-search__close');
 
 //   formSearch?.addEventListener('click', function (e) {
 //     formSearch.classList.toggle('open');
@@ -10,14 +31,15 @@
 //     logo.classList.toggle('hidden');
 //   });
 
-//   formClose?.forEach(el => {
-//     el.addEventListener('click', () => {
+//   if (formClose) {
+//     formClose.addEventListener('click', function () {
 //       logo.classList.remove('hidden');
 //       headerAction.classList.remove('hidden');
 //       formSearch.classList.remove('open');
 //     });
-//   });
+//   }
 // })();
+
 
 (function () {
   const logo = document.querySelector('[data-logo]');
@@ -31,13 +53,10 @@
     logo.classList.toggle('hidden');
   });
 
-  if (formClose) {
-    formClose.addEventListener('click', function () {
-      logo.classList.remove('hidden');
-      headerAction.classList.remove('hidden');
-      formSearch.classList.remove('open');
-    });
-  }
+  formClose?.addEventListener('click', function (e) {
+    logo.classList.remove('hidden');
+    headerAction.classList.remove('hidden');
+    formSearch.classList.remove('open');
+  });
 })();
-
 
