579115604fd48aeff8ffc9e6e470f9ad33d21b4d
BAO-1987 <andriy.bezkorovajnyj@gmail.com>
2023-06-25 19:10:30 +0300

    ddsdsdsss

--- app/js/main.js
+++ app/js/main.js
@@ -546,62 +546,22 @@ showPasswordCheckboxes.forEach(function (checkbox) {
   \**************************************/
 /***/ (() => {
 
-// // (function () {
-// //   const logo = document.querySelector('[data-logo]');
-// //   const headerAction = document.querySelector('[data-hide]');
-// //   const formSearch = document.querySelector('[data-search]');
-// //   const formClose = document.querySelector('[data-close]');
-
-// //   formSearch?.addEventListener('click', function (e) {
-// //     formSearch.classList.toggle('open');
-// //     headerAction.classList.toggle('hidden');
-// //     logo.classList.toggle('hidden');
-// //   });
-
-// //   formClose?.forEach(el => {
-// //     el.addEventListener('click', () => {
-// //       logo.classList.remove('hidden');
-// //       headerAction.classList.remove('hidden');
-// //       formSearch.classList.remove('open');
-// //     });
-// //   });
-// // })();
-
-// (function () {
-//   const logo = document.querySelector('[data-logo]');
-//   const headerAction = document.querySelector('[data-hide]');
-//   const formSearch = document.querySelector('[data-search]');
-//   const formClose = document.querySelector('form-search__close');
-
-//   formSearch?.addEventListener('click', function (e) {
-//     formSearch.classList.toggle('open');
-//     headerAction.classList.toggle('hidden');
-//     logo.classList.toggle('hidden');
-//   });
-
-//   if (formClose) {
-//     formClose.addEventListener('click', function () {
-//       logo.classList.remove('hidden');
-//       headerAction.classList.remove('hidden');
-//       formSearch.classList.remove('open');
-//     });
-//   }
-// })();
-
 (function () {
   const logo = document.querySelector('[data-logo]');
   const headerAction = document.querySelector('[data-hide]');
   const formSearch = document.querySelector('[data-search]');
   const formClose = document.querySelector('form-search__close');
-  formSearch?.addEventListener('click', function (e) {
+  formSearch?.addEventListener('click', e => {
     formSearch.classList.toggle('open');
     headerAction.classList.toggle('hidden');
     logo.classList.toggle('hidden');
   });
-  formClose?.addEventListener('click', function (e) {
-    logo.classList.remove('hidden');
-    headerAction.classList.remove('hidden');
-    formSearch.classList.remove('open');
+  formClose?.forEach(el => {
+    el.addEventListener('click', () => {
+      logo.classList.remove('hidden');
+      headerAction.classList.remove('hidden');
+      formSearch.classList.remove('open');
+    });
   });
 })();
 
