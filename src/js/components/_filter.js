a9c9a31a5f3a5aeb58abb75d5bc480103c352f98
BAO-1987 <andriy.bezkorovajnyj@gmail.com>
2023-08-13 21:45:39 +0300

    vhgvghv

--- src/js/components/_filter.js
+++ src/js/components/_filter.js
@@ -1,48 +1,169 @@
-// const filters = document.querySelectorAll('.filter');
-
-// filters.forEach(filter => {
-
-//   filter.addEventListener('click', function () {
-
-//     let selectedFilter = filter.getAttribute('data-filter');
-//     let itemsToHide = document.querySelectorAll(`.single-reviews__list .single-reviews__item:not([data-filter='${selectedFilter}'])`);
-//     let itemsToShow = document.querySelectorAll(`.single-reviews__list[data-filter='${selectedFilter}']`);
-
-//     if (selectedFilter == 'all') {
-//       itemsToHide = [];
-//       itemsToShow = document.querySelectorAll('.single-reviews__list [data-filter]');
-//     }
-
-//     itemsToHide.forEach(el => {
-//       el.classList.add('hide');
-//       el.classList.remove('show');
-//     });
-
-//     itemsToShow.forEach(el => {
-//       el.classList.remove('hide');
-//       el.classList.add('show');
-//     });
-
+// const showButton = document.querySelector("[data-filters-show]");
+// const hideButton = document.querySelector("[data-filters-hide]");
+// const filtersColumns = document.querySelector("[data-filters-columns]");
+// if (filtersColumns) {
+//   hideButton.addEventListener("click", function (event) {
+//     const targetSelector = event.target.dataset.filtersHide;
+//     hideButton.classList.remove("hide");
+//     showButton.classList.remove("show");
+//     document.querySelector(targetSelector).classList.add("show");
+//     filtersColumns.className = "catalog__content";
 //   });
 
-// const filters = document.querySelectorAll('.filter');
-// const items = document.querySelectorAll('.single-reviews__item');
+//   showButton.addEventListener("click", function (event) {
+//     const targetSelector = event.target.dataset.filtersShow;
+//     hideButton.classList.add("show");
+//     showButton.classList.add("hide");
+//     document.querySelector(targetSelector).classList.remove("show");
+//     filtersColumns.className = "catalog__content";
+//   });
+// }
 
-// filters.forEach(filter => {
-//   filter.addEventListener('click', function () {
-//     const selectedFilter = filter.getAttribute('data-filter');
+// const showButton = document.querySelector("[data-filters-show]");
+// const hideButton = document.querySelector("[data-filters-hide]");
+// const columnsContainer = document.querySelector("[data-filters-columns]");
 
-//     items.forEach(item => {
-//       if (selectedFilter === 'all' || item.getAttribute('data-filter') === selectedFilter) {
-//         item.classList.remove('hide');
-//         item.classList.add('show');
-//       } else {
-//         item.classList.remove('show');
-//         item.classList.add('hide');
+// if (columnsContainer !== null) {
+//   hideButton.addEventListener("click", function (event) {
+//     const targetSelector = event.target.dataset.filtersHide;
+//     hideButton.classList.remove("show");
+//     showButton.classList.remove("hide");
+//     document.querySelector(targetSelector).classList.add("hide");
+//     columnsContainer.className = "catalog__content";
+//   });
+
+//   showButton.addEventListener("click", function (event) {
+//     const targetSelector = event.target.dataset.filtersShow;
+//     hideButton.classList.add("show");
+//     showButton.classList.add("hide");
+//     document.querySelector(targetSelector).classList.remove("hide");
+//     columnsContainer.className = "catalog__content";
+//   });
+// }
+
+// const showButton = document.querySelector("[data-filters-show]");
+// const hideButton = document.querySelector("[data-filters-hide]");
+// const columnsContainer = document.querySelector("[data-filters-columns]");
+
+// if (columnsContainer) {
+//   const toggleColumns = (targetSelector, hide) => {
+//     const targetElement = document.querySelector(targetSelector);
+//     if (targetElement) {
+//       targetElement.classList.toggle("hide", hide);
+//     }
+//     hideButton.classList.toggle("show", hide);
+//     showButton.classList.toggle("hide", !hide);
+//   };
+
+//   hideButton.addEventListener("click", (event) => {
+//     toggleColumns(event.target.dataset.filtersHide, true);
+//   });
+
+//   showButton.addEventListener("click", (event) => {
+//     toggleColumns(event.target.dataset.filtersShow, false);
+//   });
+// }
+
+
+// const showButton = document.querySelector("[data-filters-show]");
+// const hideButton = document.querySelector("[data-filters-hide]");
+// const columnsContainer = document.querySelector("[data-filters-columns]");
+
+// if (columnsContainer) {
+//   const toggleColumns = (targetSelector, show) => {
+//     const targetElement = document.querySelector(targetSelector);
+//     if (targetElement) {
+//       targetElement.classList.toggle("hide", !show);
+//     }
+//     hideButton.classList.toggle("show", show);
+//     showButton.classList.toggle("hide", !show);
+//   };
+
+//   hideButton.addEventListener("click", (event) => {
+//     toggleColumns(event.target.dataset.filtersHide, false);
+//   });
+
+//   showButton.addEventListener("click", (event) => {
+//     toggleColumns(event.target.dataset.filtersShow, true);
+//   });
+// }
+
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   const showButtons = document.querySelectorAll("[data-filters-show]");
+//   const hideButtons = document.querySelectorAll("[data-filters-hide]");
+//   const columnsContainer = document.querySelector("[data-filters-columns]");
+
+//   // showButtons.forEach((showButton) => {
+//   //   showButton.addEventListener("click", () => {
+//   //     const targetSelector = showButton.dataset.filtersShow;
+//   //     const targetElement = document.querySelector(targetSelector);
+//   //     if (targetElement) {
+//   //       targetElement.classList.remove("hide");
+//   //     }
+//   //     hideButton.classList.add("show");
+//   //     showButton.classList.add("hide");
+//   //   });
+//   // });
+
+//   // hideButtons.forEach((hideButton) => {
+//   //   hideButton.addEventListener("click", () => {
+//   //     const targetSelector = hideButton.dataset.filtersHide;
+//   //     const targetElement = document.querySelector(targetSelector);
+//   //     if (targetElement) {
+//   //       targetElement.classList.add("hide");
+//   //     }
+//   //     hideButton.classList.remove("show");
+//   //     showButton.classList.remove("hide");
+//   //   });
+//   // });
+
+//   showButtons.forEach(button => {
+//     button.addEventListener("click", function () {
+//       const targetSelector = button.getAttribute("data-filters-show");
+//       const targetElement = document.querySelector(targetSelector);
+//       if (targetElement) {
+//         targetElement.style.display = "block";
+//       }
+//     });
+//   });
+
+//   hideButtons.forEach(button => {
+//     button.addEventListener("click", function () {
+//       const targetSelector = button.getAttribute("data-filters-hide");
+//       const targetElement = document.querySelector(targetSelector);
+//       if (targetElement) {
+//         targetElement.style.display = "none";
 //       }
 //     });
 //   });
 // });
 
+// Query DOM elements
+const showFiltersButton = document.querySelector("[data-filters-show]");
+const hideFiltersButton = document.querySelector("[data-filters-hide]");
+const filtersColumnsElement = document.querySelector("[data-filters-columns]");
 
+if (filtersColumnsElement) {
+  // Add event listener for hiding filters
+  hideFiltersButton.addEventListener("click", function (event) {
+    const targetSelector = event.target.dataset.filtersHide;
 
+    hideFiltersButton.classList.remove("show");
+    showFiltersButton.classList.remove("hide");
+    document.querySelector(targetSelector).classList.add("hide");
+
+    filtersColumnsElement.className = "catalog__content active";
+  });
+
+  // Add event listener for showing filters
+  showFiltersButton.addEventListener("click", function (event) {
+    const targetSelector = event.target.dataset.filtersShow;
+
+    hideFiltersButton.classList.add("show");
+    showFiltersButton.classList.add("hide");
+    document.querySelector(targetSelector).classList.remove("hide");
+
+    filtersColumnsElement.className = "catalog__content";
+  });
+}
