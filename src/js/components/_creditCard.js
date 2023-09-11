b7ac9ea47ad47df4c28fa681de4801ed28f99827
BAO-1987 <andriy.bezkorovajnyj@gmail.com>
2023-09-11 22:31:38 +0300

    fsffsfsf

--- src/js/components/_creditCard.js
+++ src/js/components/_creditCard.js
@@ -1,134 +1,56 @@
-const expirationSelect = document.querySelector("[data-expiration-year]")
-const logo = document.querySelector("[data-logo]")
+import Cleave from 'cleave.js';
 
-const currentYear = new Date().getFullYear()
-for (let i = currentYear; i < currentYear + 10; i++) {
-  const option = document.createElement("option")
-  option.value = i
-  option.innerText = i
-  expirationSelect.append(option)
-}
 
-document.addEventListener("keydown", e => {
-  const input = e.target
-  const key = e.key
-  if (!isConnectedInput(input)) return
+(function () {
+  const elements = document.querySelectorAll("[data-format]");
 
-  switch (key) {
-    case "ArrowLeft": {
-      if (input.selectionStart === 0 && input.selectionEnd === 0) {
-        const prev = input.previousElementSibling
-        prev.focus()
-        prev.selectionStart = prev.value.length - 1
-        prev.selectionEnd = prev.value.length - 1
-        e.preventDefault()
-      }
-      break
+  elements.forEach((element) => {
+    const format = element.dataset.format;
+    let blocks = element.dataset.blocks || '';
+    let delimiter = element.dataset.delimiter || ' ';
+
+    switch (format) {
+      case "card":
+        new Cleave(element, {
+          creditCard: true,
+          onCreditCardTypeChanged: function (type) {
+            document.querySelector('.type').innerHTML = type;
+          }
+        });
+        break;
+      case "cvc":
+        new Cleave(element, {
+          numeral: true,
+          numeralIntegerScale: 3
+        });
+        break;
+      case "date":
+        new Cleave(element, {
+          date: true,
+          datePattern: ["m", "y"]
+        });
+        break;
+      case "date-long":
+        new Cleave(element, {
+          date: true,
+          delimiter: "-",
+          datePattern: ["Y", "m", "d"]
+        });
+        break;
+      case "time":
+        new Cleave(element, {
+          time: true,
+          datePattern: ["h", "m"]
+        });
+        break;
+      case "custom":
+        new Cleave(element, {
+          delimiter: delimiter,
+          blocks: blocks ? blocks.split(" ").map(Number) : ''
+        });
+        break;
+      default:
+        throw new Error(`Sorry, your format ${format} is not available. You can add it to the theme object method - inputFormatter in src/js/theme.js or choose one from the list of available formats: card, cvc, date, date-long, time, or custom.`);
     }
-    case "ArrowRight": {
-      if (
-        input.selectionStart === input.value.length &&
-        input.selectionEnd === input.value.length
-      ) {
-        const next = input.nextElementSibling
-        next.focus()
-        next.selectionStart = 1
-        next.selectionEnd = 1
-        e.preventDefault()
-      }
-      break
-    }
-    case "Delete": {
-      if (
-        input.selectionStart === input.value.length &&
-        input.selectionEnd === input.value.length
-      ) {
-        const next = input.nextElementSibling
-        next.value = next.value.substring(1, next.value.length)
-        next.focus()
-        next.selectionStart = 0
-        next.selectionEnd = 0
-        e.preventDefault()
-      }
-      break
-    }
-    case "Backspace": {
-      if (input.selectionStart === 0 && input.selectionEnd === 0) {
-        const prev = input.previousElementSibling
-        prev.value = prev.value.substring(0, prev.value.length - 1)
-        prev.focus()
-        prev.selectionStart = prev.value.length
-        prev.selectionEnd = prev.value.length
-        e.preventDefault()
-      }
-      break
-    }
-    default: {
-      if (e.ctrlKey || e.altKey) return
-      if (key.length > 1) return
-      if (key.match(/^[^0-9]$/)) return e.preventDefault()
-
-      e.preventDefault()
-      onInputChange(input, key)
-    }
-  }
-})
-
-document.addEventListener("paste", e => {
-  const input = e.target
-  const data = e.clipboardData.getData("text")
-
-  if (!isConnectedInput(input)) return
-  if (!data.match(/^[0-9]+$/)) return e.preventDefault()
-
-  e.preventDefault()
-  onInputChange(input, data)
-})
-
-function onInputChange(input, newValue) {
-  const start = input.selectionStart
-  const end = input.selectionEnd
-  updateInputValue(input, newValue, start, end)
-  focusInput(input, newValue.length + start)
-  const firstFour = input
-    .closest("[data-connected-inputs]")
-    .querySelector("input").value
-
-  if (firstFour.startsWith("4")) {
-    logo.src = "./img/visa.svg"
-  } else if (firstFour.startsWith("5")) {
-    logo.src = "./img/master-card.svg"
-  }
-}
-
-function updateInputValue(input, extraValue, start = 0, end = 0) {
-  const newValue = `${input.value.substring(
-    0,
-    start
-  )}${extraValue}${input.value.substring(end, 4)}`
-  input.value = newValue.substring(0, 4)
-  if (newValue > 4) {
-    const next = input.nextElementSibling
-    if (next == null) return
-    updateInputValue(next, newValue.substring(4))
-  }
-}
-
-function focusInput(input, dataLength) {
-  let addedChars = dataLength
-  let currentInput = input
-  while (addedChars > 4 && currentInput.nextElementSibling != null) {
-    addedChars -= 4
-    currentInput = currentInput.nextElementSibling
-  }
-  if (addedChars > 4) addedChars = 4
-
-  currentInput.focus()
-  currentInput.selectionStart = addedChars
-  currentInput.selectionEnd = addedChars
-}
-
-function isConnectedInput(input) {
-  const parent = input.closest("[data-connected-inputs]")
-  return input.matches("input") && parent != null
-}
+  });
+})();
