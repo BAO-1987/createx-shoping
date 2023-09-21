1842812d246890d1f560bf8020a8512dfc4df3e5
BAO-1987 <andriy.bezkorovajnyj@gmail.com>
2023-09-21 21:38:42 +0300

    dddd

--- app/js/main.js
+++ app/js/main.js
@@ -1471,22 +1471,15 @@ AddTableARIA();
   \************************************/
 /***/ (() => {
 
-// const parentElement = document.querySelector('.parent-element'); // Replace 'parent-element' with the actual parent element containing like buttons
+const grid = document.querySelector(".services__list");
+// const cards = document.querySelectorAll(".card");
 
-// function toggleLike(event) {
-//   const target = event.target;
+// const gridSize = grid.getBoundingClientRect();
 
-//   if (target.classList.contains('article__wish')) {
-//     if (!target.classList.contains('is-liked')) {
-//       target.classList.add('is-liked');
-//     } else {
-//       target.classList.remove('is-liked');
-//     }
-//   }
-// }
-
-// // Add a single click event listener to the parent element
-// parentElement.addEventListener('click', toggleLike);
+document.addEventListener("mousemove", e => {
+  grid.style.setProperty("--x", e.x + "px");
+  grid.style.setProperty("--y", e.y + "px");
+});
 
 /***/ }),
 
