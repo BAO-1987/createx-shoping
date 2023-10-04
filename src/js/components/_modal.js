cb65522c1a83ad754d2533553bf8b18ea6519745
BAO-1987 <andriy.bezkorovajnyj@gmail.com>
2023-10-04 20:53:41 +0300

    sfsfs

--- src/js/components/_modal.js
+++ src/js/components/_modal.js
@@ -1,35 +1,88 @@
+// const buttons = document.querySelectorAll(".modal__btn");
+// const page = document.querySelector(".page");
+// const closeButtons = document.querySelectorAll("[data-modal-close]");
+// let currentModal = null;
+
+// buttons.forEach((button) => {
+//   button.addEventListener("click", (event) => {
+//     const modalId = event.target.dataset.modal;
+//     const modal = document.getElementById(modalId);
+//     if (modal && modal !== currentModal) {
+//       if (currentModal) {
+//         currentModal.close();
+//       }
+//       modal.showModal();
+//       currentModal = modal;
+//       page.classList.add("dis-scroll");
+//     }
+//   });
+// });
+
+// closeButtons.forEach((button) => {
+//   button.addEventListener("click", (event) => {
+//     const modal = event.target.closest('dialog');
+//     if (modal && modal === currentModal) {
+//       modal.close();
+//       currentModal = null;
+//       page.classList.remove("dis-scroll");
+//     }
+//   });
+// });
+
+
+
+
 const buttons = document.querySelectorAll(".modal__btn");
 const page = document.querySelector(".page");
-
 const closeButtons = document.querySelectorAll("[data-modal-close]");
+const modals = document.querySelectorAll("dialog");
 let currentModal = null;
+
+function openModal(modal) {
+  if (modal && modal !== currentModal) {
+    if (currentModal) {
+      closeModal(currentModal);
+    }
+    modal.showModal();
+    currentModal = modal;
+    page.classList.add("dis-scroll");
+  }
+}
+
+function closeModal(modal) {
+  if (modal) {
+    modal.setAttribute("closing", "");
+
+    modal.addEventListener(
+      "animationend",
+      () => {
+        modal.removeAttribute("closing");
+        modal.close();
+      }, {
+        once: true
+      }
+    );
+
+    page.classList.remove("dis-scroll");
+  }
+}
 
 buttons.forEach((button) => {
   button.addEventListener("click", (event) => {
     const modalId = event.target.dataset.modal;
     const modal = document.getElementById(modalId);
-    if (modal && modal !== currentModal) {
-      if (currentModal) {
-        currentModal.close();
-      }
-      modal.showModal();
-      currentModal = modal;
-      page.classList.add("dis-scroll");
-    }
+    openModal(modal);
   });
 });
 
 closeButtons.forEach((button) => {
-  button.addEventListener("click", (event) => {
-    const modal = event.target.closest('dialog');
-    if (modal && modal === currentModal) {
-      modal.close();
-      currentModal = null;
-      page.classList.remove("dis-scroll");
-    }
+  button.addEventListener("click", () => {
+    closeModal(currentModal);
   });
 });
 
-
-
-
+modals.forEach((modal) => {
+  modal.addEventListener("close", () => {
+    currentModal = null;
+  });
+});
