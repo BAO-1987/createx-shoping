4e1670b90114e77876dd2c71fd239941c640e129
BAO-1987 <andriy.bezkorovajnyj@gmail.com>
2023-10-08 14:56:08 +0300

    sdsss

--- src/js/components/_animation.js
+++ src/js/components/_animation.js
@@ -12,7 +12,7 @@ ScrollReveal().reveal(".title", {
   origin: "top",
 });
 
-ScrollReveal().reveal(".hero__title, .title--banners", {
+ScrollReveal().reveal(".hero__title, .title--banners, .modal__title, .single-reviews__title", {
   distance: '0',
   delay: 0,
   duration: 0,
@@ -24,7 +24,6 @@ ScrollReveal().reveal(".hero__title, .title--banners", {
 ScrollReveal().reveal(".arrival__text, .catalog__nav, .single-blog__content", {
   duration: 1000,
   distance: "20px",
-  delay: "100",
   origin: "top",
 });
 
@@ -35,7 +34,14 @@ ScrollReveal().reveal(".subscribe__box", {
   origin: "right",
 });
 
-ScrollReveal().reveal(".slider__btn, .btn--instagram, .checkout-form__aside, .single-blog__article, .form--blog", {
+ScrollReveal().reveal(".footer", {
+  duration: 1000,
+  distance: "20px",
+  delay: "100",
+  origin: "bottom",
+});
+
+ScrollReveal().reveal(".slider__btn, .btn--instagram, .single-blog__article, .form--blog, .form--subscribe", {
   duration: 800,
   distance: "30px",
   delay: "500",
@@ -68,111 +74,6 @@ ScrollReveal().reveal(
     origin: "top",
   }
 );
-
-// ScrollReveal().reveal(".gallery__list> li", {
-//   duration: 500,
-//   distance: "10%",
-//   delay: "100",
-//   origin: "top",
-//   interval: 200,
-//   opacity: 0,
-// });
-
-
-// ScrollReveal().reveal(
-//   ".services__more, .news__more, .reviews__more, .gallery__more, .slider-btn, .btn--second", {
-//     duration: 500,
-//     distance: "10px",
-//     delay: "100",
-//     origin: "top",
-//   }
-// );
-
-
-// ScrollReveal().reveal(".contacts__inner, .pagination", {
-//   duration: 700,
-//   distance: "10px",
-//   delay: "500",
-//   origin: "top",
-// });
-
-// ScrollReveal().reveal(".tabs__nav> li", {
-//   duration: 500,
-//   distance: "10px",
-//   easing: "ease",
-//   delay: "100",
-//   origin: "top",
-//   interval: 200,
-//   opacity: 1,
-//   mobile: false,
-// });
-
-
-// ScrollReveal().reveal(".footer", {
-//   duration: 800,
-//   distance: "10px",
-//   easing: "ease",
-//   delay: "500",
-//   origin: "bottom",
-// });
-
-// ScrollReveal().reveal(".title", {
-//   duration: 500,
-//   distance: "20px",
-//   easing: "ease",
-//   delay: "200",
-//   origin: "top",
-// });
-
-// ScrollReveal().reveal(".dentures__title., title--second", {
-//   duration: 600,
-//   distance: "20px",
-//   easing: "ease",
-//   delay: "150",
-//   origin: "top",
-// });
-
-// ScrollReveal().reveal(
-//   ".advantages__list, .dentures__list, .services__list, .investment__list, .stages__list", {
-//     duration: 600,
-//     distance: "20px",
-//     easing: "ease",
-//     delay: "100",
-//     origin: "top",
-//   }
-// );
-
-
-//  function isMobile() {
-//    return window.innerWidth <= 768; // Adjust the threshold as needed
-//  }
-
-//  // Initialize ScrollReveal and apply animations conditionally
-//  function setupScrollReveal() {
-//    const sr = ScrollReveal({
-//      duration: 1000, // Set your animation duration
-//      reset: true, // Reset the animation when the element is not in the viewport
-//    });
-
-//    const revealConfig = {
-//      origin: 'bottom', // Change this to your desired animation origin
-//      distance: '20px', // Change this to your desired animation distance
-//    };
-
-//    const elementsToAnimate = '.title--banners';
-
-//    // Check if it's not a mobile device, then set the elements you want to animate
-//    if (!isMobile()) {
-//      sr.reveal(elementsToAnimate, revealConfig);
-//    }
-//  }
-
-//  // Call the function to set up ScrollReveal and animations on load
-//  setupScrollReveal();
-
-//  // You can also call the function whenever you need to check for mobile and set up animations
-//  // For example, you could call it on window resize:
-//  window.addEventListener('resize', setupScrollReveal);
 
 
 const sr = ScrollReveal({
